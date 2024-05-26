6ece4ead6717e827cbf23124a6d656871c6472ff
dhiraj0911 <djirajbari123@gmail.com>
2024-05-27 00:34:40 +0530

    only manvi's code

--- profanity-checker/src/components/ProfanityChecker.vue
+++ profanity-checker/src/components/ProfanityChecker.vue
@@ -53,7 +53,7 @@ export default {
  methods: {
     async checkProfanity() {
       try {
-        const response = await fetch('http://127.0.0.1:5000/profanity-check', {
+        const response = await fetch('http://18.210.16.147:5000/profanity-check', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json'
